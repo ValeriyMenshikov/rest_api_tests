f3d87226455fd43ee4927b713cd1623c2b7f9bd9
ValeriyMenshikov <team095hunter@gmail.com>
2023-02-10 00:46:51 +0300

    mailhog и тест

---
+++ services/mailhog.py
@@ -0,0 +1,35 @@
+import json
+import time
+
+from requests import session, Response
+
+
+class MailhogApi:
+    def __init__(self, host='http://localhost:5025'):
+        self.host = host
+        self.session = session()
+
+    def get_api_v2_messages(self, limit: int = 50) -> Response:
+        """
+        Get messages by limit
+        :param limit:
+        :return:
+        """
+        response = self.session.get(
+            url=f"{self.host}/api/v2/messages",
+            params={
+                'limit': limit
+            }
+        )
+
+        return response
+
+    def get_token_from_last_email(self) -> str:
+        """
+        Get user activation token from last email
+        :return:
+        """
+        emails = self.get_api_v2_messages(limit=1).json()
+        token_url = json.loads(emails['items'][0]['Content']['Body'])['ConfirmationLinkUrl']
+        token = token_url.split('/')[-1]
+        return token
--- tests/test_post_v1_account.py
+++ tests/test_post_v1_account.py
@@ -1,15 +1,18 @@
 import requests
 from services.dm_api_account import DmApiAccount
+from services.mailhog import MailhogApi
 
 
 def test_post_v1_account():
+    mailhog = MailhogApi(host='http://localhost:5025')
     api = DmApiAccount(host='http://localhost:5051')
     json = {
         "login": "login_8",
         "email": "login_8@mail.ru",
         "password": "login_8"
     }
-    response = api.account.post_v1_account(
-        json=json
-    )
+    response = api.account.post_v1_account(json=json)
+    token = mailhog.get_token_from_last_email()
+    response = api.account.put_v1_account_token(token=token)
     print(response)
+    print(response.json())
