b6531827deb42b233f496c35d0d158c7399d7ead
ValeriyMenshikov <team095hunter@gmail.com>
2023-02-19 23:48:54 +0300

    правки в логин апи

--- dm_api_account/apis/login_api.py
+++ dm_api_account/apis/login_api.py
@@ -11,9 +11,10 @@ class LoginApi:
         if headers:
             self.client.session.headers.update(headers)
 
-    def post_v1_account_login(self, json: LoginCredentials) -> Response:
+    def post_v1_account_login(self, json: LoginCredentials, status_code: int = 200) -> Response:
         """
         Authenticate via credentials
+        :param status_code:
         :param json: login_credentials_model
         :return:
         """
@@ -22,9 +23,10 @@ class LoginApi:
             path=f"/v1/account/login",
             json=validate_request_json(json)
         )
+        validate_status_code(response, status_code)
         return response
 
-    def delete_v1_account_login(self, **kwargs) -> Response:
+    def delete_v1_account_login(self, status_code: int = 204, **kwargs, ) -> Response:
         """
         Logout as current user
         :return:
@@ -33,9 +35,10 @@ class LoginApi:
             path=f"/v1/account/login",
             **kwargs
         )
+        validate_status_code(response, status_code)
         return response
 
-    def delete_v1_account_login_all(self, **kwargs) -> Response:
+    def delete_v1_account_login_all(self, status_code: int = 204, **kwargs) -> Response:
         """
         Logout from every device
         :return:
@@ -44,5 +47,5 @@ class LoginApi:
             path=f"/v1/account/login/all",
             **kwargs
         )
-
+        validate_status_code(response, status_code)
         return response
