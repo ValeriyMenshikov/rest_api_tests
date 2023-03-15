ce5ed05cb8dcdd88ecd6c1f8e30c669c328ee4e9
ValeriyMenshikov <team095hunter@gmail.com>
2023-03-15 22:21:57 +0300

    работа с конфигом

--- conftest.py
+++ conftest.py
@@ -1,8 +1,10 @@
 import pytest
+import structlog
+from vyper import v
+from pathlib import Path
 from generic.helpers.mailhog import MailhogApi
 from generic.helpers.dm_db import DmDatabase
 from services.dm_api_account import Facade
-import structlog
 
 structlog.configure(
     processors=[
@@ -13,15 +15,47 @@ structlog.configure(
 
 @pytest.fixture
 def mailhog():
-    return MailhogApi(host='http://localhost:5025')
+    return MailhogApi(host=v.get('service.mailhog'))
 
 
 @pytest.fixture
 def dm_api_facade(mailhog):
-    return Facade(host='http://localhost:5051', mailhog=mailhog)
+    return Facade(
+        host=v.get('service.dm_api_account'),
+        mailhog=mailhog
+    )
+
+
+options = (
+    'service.dm_api_account',
+    'service.mailhog',
+    'database.dm3_5.host',
+)
 
 
 @pytest.fixture
 def dm_db():
-    db = DmDatabase(user='postgres', password='admin', host='localhost', database='dm3.5')
+    db = DmDatabase(
+        user=v.get('database.dm3_5.user'),
+        password=v.get('database.dm3_5.password'),
+        host=v.get('database.dm3_5.host'),
+        database=v.get('database.dm3_5.database')
+    )
     return db
+
+
+@pytest.fixture(autouse=True)
+def set_config(request):
+    config = Path(__file__).parent.joinpath('config')
+    config_name = request.config.getoption('--env')
+    v.set_config_name(config_name)
+    v.add_config_path(config)
+    v.read_in_config()
+    for option in options:
+        v.set(option, request.config.getoption(f'--{option}'))
+
+
+def pytest_addoption(parser):
+    parser.addoption('--env', action='store', default='stg')
+    for option in options:
+        parser.addoption(f'--{option}', action='store', default=None)
