ea8cb3df0b926b49700bd67d9b47868e6e0b059b
Menshikov Valeriy Sergeevich <vmenshikov@ozon.ru>
2023-05-26 13:08:52 +0300

    fix

--- tests/test_account/test_post_v1_account.py
+++ tests/test_account/test_post_v1_account.py
@@ -20,13 +20,23 @@ def random_string(begin=1, end=30):
 @allure.suite("Тесты на проверку метода POST{host}/v1/account")
 @allure.sub_suite("Позитивные проверки")
 class TestsPostV1Account:
-    @pytest.mark.parametrize('login, email, password, status_code, check', [
-        (random_string(2), f'{random_string()}@{random_string()}.{random_string()}', random_string(6), 201, ''),
-        (random_string(2), f'{random_string()}@{random_string()}.{random_string()}', random_string(1, 5), 400, {"Password": ["Short"]}),
-        (random_string(1, 1), f'{random_string()}@{random_string()}.{random_string()}', random_string(6), 400, {"Login": ["Short"]}),
-        (random_string(2), f'{random_string(6)}@', random_string(6), 400, {"Email": ["Invalid"]}),
-        (random_string(2), random_string(1, 2), random_string(6), 400, {"Email": ["Invalid"]}),
-    ])
+    random_email = f'{random_string()}@{random_string()}.{random_string()}'
+    valid_login = random_string(2)
+    invalid_login = random_string(1, 1)
+    valid_password = random_string(6)
+    invalid_password = random_string(1, 5)
+    invalid_email = f'{random_string(6)}@'
+    invalid_email_1 = random_string(1, 2).replace('@', '')
+
+    random_data = [
+        (valid_login, random_email, valid_password, 201, ''),
+        (valid_login, random_email, invalid_password, 400, {"Password": ["Short"]}),
+        (invalid_login, random_email, valid_password, 400, {"Login": ["Short"]}),
+        (valid_login, invalid_email, valid_password, 400, {"Email": ["Invalid"]}),
+        (valid_login, invalid_email_1, valid_password, 400, {"Email": ["Invalid"]}),
+    ]
+
+    @pytest.mark.parametrize('login, email, password, status_code, check', random_data)
     def test_create_and_activated_user_with_random_params(
             self,
             dm_api_facade,
