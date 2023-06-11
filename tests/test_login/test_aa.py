45bb1c229fdd03e3e4e5408105311fc87a053a2f
Menshikov Valeriy Sergeevich <vmenshikov@ozon.ru>
2023-06-11 19:38:31 +0300

    requirements.txt

--- tests/test_login/test_aa.py
+++
@@ -1,6 +0,0 @@
-from generic.helpers.orm_db import OrmDatabase
-
-
-def test_db():
-    OrmDatabase(host='localhost', database='dm3.5', user='postgres', password='admin').get_all_users()
-    # dm_db.get_all_users()
